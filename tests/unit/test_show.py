cbb4ba9f3c7217b30aec057c7b8c886cc7970c68
shoito <shoito@users.noreply.github.com>
2018-06-05 00:17:33 +0900

    Show APIの実装とユニットテスト
    - TODOの取得に該当するAPI GET /todos/{id}
    - AWS SAMで管理するためtemplate.yml実装

---
+++ tests/unit/test_show.py
@@ -0,0 +1,90 @@
+import json
+import os
+import pytest
+import todos.show as show
+
+from botocore.exceptions import ClientError
+from todos.utils import dynamodb, table
+
+TABLE_NAME = os.getenv('TABLE_NAME')
+
+
+def setup():
+    try:
+        dynamodb().create_table(
+            TableName=TABLE_NAME,
+            KeySchema=[
+                {
+                    'AttributeName': 'id',
+                    'KeyType': 'HASH'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'id',
+                    'AttributeType': 'S'
+                },
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 1,
+                'WriteCapacityUnits': 1
+            }
+        )
+
+        table(TABLE_NAME).put_item(
+            Item={
+                'id': '1',
+                'title': 'test',
+                'descr': 'test',
+                'deadline': '2018-06-30',
+                'status': 'TODO'
+            }
+        )
+    except ClientError:
+        pass
+
+
+def teardown():
+    try:
+        table(TABLE_NAME).delete()
+    except ClientError:
+        pass
+
+
+@pytest.fixture()
+def event_todo_exists():
+    return {
+        'body': '{}',
+        'queryStringParameters': {
+        },
+        'pathParameters': {
+            'id': '1'
+        },
+        'httpMethod': 'POST',
+    }
+
+
+def test_todo_exists(event_todo_exists):
+    ret = show.lambda_handler(event_todo_exists, '')
+    assert ret['statusCode'] == 200
+
+    data = json.loads(ret['body'])
+    assert data['id'] == '1'
+
+
+@pytest.fixture()
+def event_todo_not_found():
+    return {
+        'body': '{}',
+        'queryStringParameters': {
+        },
+        'pathParameters': {
+            'id': '999'
+        },
+        'httpMethod': 'POST',
+    }
+
+
+def test_todo_not_found(event_todo_not_found):
+    ret = show.lambda_handler(event_todo_not_found, '')
+    assert ret['statusCode'] == 404
---
+++ todos/utils.py
@@ -0,0 +1,35 @@
+import boto3
+import json
+import logging
+import os
+import sys
+
+logger = logging.getLogger()
+
+DEFAULT_REGION = os.getenv('DEFAULT_REGION', None) # ap-northeast-1
+DYNAMODB_ENDPOINT = os.getenv('DYNAMODB_ENDPOINT', None) # https://dynamodb.ap-northeast-1.amazonaws.com/
+
+if DEFAULT_REGION is None:
+    logger.critical('Could not find DEFAULT_REGION env.')
+    sys.exit(1)
+if DYNAMODB_ENDPOINT is None:
+    logger.critical('Could not find DYNAMODB_ENDPOINT env.')
+    sys.exit(1)
+
+
+def dynamodb():
+    return boto3.resource('dynamodb', region_name=DEFAULT_REGION, endpoint_url=DYNAMODB_ENDPOINT)
+
+
+def table(table_name):
+    return dynamodb().Table(table_name)
+
+
+def respond(err, res=None):
+    return {
+        'statusCode': err['code'] if err else 200,
+        'body': err if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        }
+    }
