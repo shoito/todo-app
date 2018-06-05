570254658357fee60ff29e61e4a1878ae0a78ce8
shoito <shoito@users.noreply.github.com>
2018-06-05 20:00:27 +0900

    Create APIの実装とユニットテスト

---
+++ todos/create.py
@@ -0,0 +1,50 @@
+import dateutil.parser
+import json
+import logging
+import os
+
+from todos.utils import respond, table
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.info('Loading function')
+
+TABLE_NAME = os.getenv('TABLE_NAME')
+
+
+def lambda_handler(event, context):
+    logger.debug('Received event: {}'.format(event))
+
+    try:
+        req = json.loads(event['body'])
+
+        if not validate(req):
+            return respond({'code': 400, 'message': 'Failed to create the todo.'})
+
+        res = table(TABLE_NAME).put_item(
+            Item={
+                'id': '1',
+                'title': req.get('title'),
+                'description': req.get('description', ''),
+                'deadline': req.get('deadline', ''),
+                'status': 'TODO'
+            }
+        )
+    except:
+        import traceback
+        traceback.print_exc()
+        return respond({'code': 400, 'message': 'Failed to create the todo.'})
+
+    return respond(None, res)
+
+
+def validate(req):
+    if not req.get('title'):
+        return False
+    if req.get('deadline'):
+        try:
+            dateutil.parser.parse(req.get('deadline'))
+        except ValueError:
+            return False
+
+    return True
